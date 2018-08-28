3143eca7b7d4cead1851769eed736723797cf1f0
Martin <42675184+tqdafachero@users.noreply.github.com>
2018-08-27 23:58:38 -0300

    Se agrega Los Stored procedure

--- DataBase Consultas/Query Crear DataBase Consultas.sql
+++ DataBase Consultas/Query Crear DataBase Consultas.sql
@@ -15,4 +15,19 @@ responsable varchar(50)
 
 drop table Clientes 
 
+--creando una restriccion(constraint) para columna codigo cliente
+alter table Clientes	
+add constraint PK_NOREPEAT
+unique ([CÓDIGO CLIENTE] ) 
+
+--quito primero la restriccion
+alter table clientes drop constraint PK_NOREPEAT;
+--quito regristo de restriccion
+alter table clientes drop constraint PK__Clientes__14F572BB03317E3D
+
+--modifico una vez sacada la restriccion
+alter table clientes alter column [CÓDIGO CLIENTE] varchar(50) not null 
+
+--agrego Restriccion Unique para que no se repita La PRIMARY KEY
+alter table clientes add constraint PK_UNIQUE unique ([CÓDIGO CLIENTE] ) 
 
---
+++ DataBase Consultas/Query sp_AgregarCliente.sql
@@ -0,0 +1,39 @@
+use Consultas
+go
+alter procedure sp_AgregarCliente 
+@cod_cliente as varchar(50),
+@empresa as varchar(50),
+@dirrecion as varchar(50),
+@poblacion as varchar(50),
+@telefono as varchar(50),
+@responsable as varchar(50),
+@msg as varchar(100) output
+
+as
+begin
+SET NOCOUNT ON
+
+ begin tran T_add
+ 
+ begin try
+	
+	insert into dbo.Clientes ([CÓDIGO CLIENTE],dirección,empresa,población,responsable,teléfono)
+	values(@cod_cliente,@empresa,@dirrecion,@poblacion,@telefono,@responsable)
+	set @msg='El usuario se registro correctamente.'
+	
+	commit tran T_add
+	 
+ end try
+ 
+ begin catch
+ set @msg= 'Ocurrio un error: '+ERROR_MESSAGE() +' en la linea '+CONVERT(nvarchar(255),error_line() )+ '.'		 
+	
+	rollback tran T_add
+ end catch
+	 
+end 
+go
+
+DECLARE @msg AS VARCHAR(100);
+EXEC sp_AgregarCliente '1','hola Torres','clau@mail.com','a220109',1,'asdasd',@msg OUTPUT
+SELECT @msg AS msg
