e9ee4cdc37f693b99e77851a5bde076a5c781e04
Martin <42675184+tqdafachero@users.noreply.github.com>
2018-09-08 16:36:22 -0300

    Add files via upload
    The elimination sp_delete is modified

--- DataBase Consultas/Query sp_EliminarCliente.sql
+++ DataBase Consultas/Query sp_EliminarCliente.sql
@@ -2,13 +2,15 @@ use Consultas
 
 create procedure sp_EliminarCliente
 (
+
 @codigo_cliente as varchar(50)
+)
 as
 begin
 SET NOCOUNT ON
+delete from Clientes where [CÓDIGO CLIENTE]=@codigo_cliente
 
-
-
+select 'el cliente a sido eliminado';
 end
 
-)
+exec sp_EliminarCliente '14';
